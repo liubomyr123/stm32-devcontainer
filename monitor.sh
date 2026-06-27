9e94d0fb70bc5ab7d5c29b3387f303e3bc0d13ee
Liubomyr <l1l2369100@gmail.com>
2026-06-27 09:13:25 +0000

    feature: added saving monitor log sessions

--- monitor.sh
+++ monitor.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-set -e
-
 echo "==> Setting permissions..."
 sudo chmod 666 /dev/ttyACM0
 
-echo "==> Starting monitor..."
-picocom -b 115200 /dev/ttyACM0
+LOGS_DIR="/workspace/logs"
+mkdir -p $LOGS_DIR
+
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+LOG_FILE="$LOGS_DIR/session_$TIMESTAMP.log"
+
+echo "==> Starting monitor... (logging to $LOG_FILE)"
+picocom -b 115200 /dev/ttyACM0 | tee $LOG_FILE
